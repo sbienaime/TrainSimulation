339dfcde506db761e8ee39dd3d582ee2fdfb1b54
unknown <Steve.Bien001@mymdc.net>
2019-07-22 10:03:33 -0400

    testing validation of getStopsFromUser() input

--- TestDrivenDev/src/TrainSimulation/Simulator.java
+++ TestDrivenDev/src/TrainSimulation/Simulator.java
@@ -8,17 +8,44 @@ package TrainSimulation;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author remyb
  */
 public class Simulator {
-
+Scanner input = new Scanner(System.in);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
     public void getStopsFromUser() {
         
+        boolean valid=false;
         
-        System.out.println("");
+        
+        System.out.println("Enter number of stops the train has on its route (must be greater than 1):  ");
+        
+        while(valid == false)
+        try{
+        
+        int stops =input.nextInt();
+        
+        valid = true;
+        
+        }
+        
+        catch(Exception ex ) {
+        
+        System.out.println("Invalid input, try again.");
+        
+        }
+       
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
